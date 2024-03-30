dd2e86aaf5340e1787e6ce28aa7646a0d1489307
rail <rail.shabayev@mail.ru>
2024-03-30 07:10:37 +0400

    feat: configure rsaKeyProperties and add jwtDecoder

--- src/main/java/com/group/demojwt/configuration/SecurityConfig.java
+++ src/main/java/com/group/demojwt/configuration/SecurityConfig.java
@@ -4,9 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -15,6 +16,12 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+
+    private final RsaKeyProperties rsaKeyProperties;
+
+    public SecurityConfig(RsaKeyProperties rsaKeyProperties) {
+        this.rsaKeyProperties = rsaKeyProperties;
+    }
 
     @Bean
     public InMemoryUserDetailsManager user() {
@@ -36,4 +43,10 @@ public class SecurityConfig {
                 .build();
 
     }
+
+    @Bean
+    JwtDecoder decoder() {
+        return NimbusJwtDecoder.withPublicKey(rsaKeyProperties.publicKey()).build();
+    }
+
 }
